c9b17166b5dcbe7b1f0098242061e86264fddcb6
Logan Liao <109937255+Logan645@users.noreply.github.com>
2022-09-30 14:47:07 +0800

    更正要求6

--- week2/week2.py
+++ week2/week2.py
@@ -96,20 +96,17 @@ print(result) # show [0, 2] because nums[0]+nums[2] is 9
 
 #要求6
 def maxZeros(nums):
-    list=[]
-    j=0
-    for i in nums:
-        if j>0 and i==0 and nums[j-1]==0:
-            list.append(nums[j])
-            j+=1
+    count= 0
+    countnote= []
+    for i in range(len(nums)):
+        if nums[i] == 0:
+            count += 1
         else:
-            j+=1
-    if nums[0]==0 and len(list)>0:
-        print(len(list)+1)
-    elif len(list)>0:
-         print(len(list))
-    else:
-        print(0)
+            countnote.append(count)
+            count = 0
+    countnote.append(count)
+
+    print(max(countnote))
 
 maxZeros([0, 1, 0, 0]) # 得到 2
 maxZeros([1, 0, 0, 0, 0, 1, 0, 1, 0, 0]) # 得到 4
