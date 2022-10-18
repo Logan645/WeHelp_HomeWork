642fbe4aa9c40a4d32f88eb0b5812acb7244e830
Logan Liao <109937255+Logan645@users.noreply.github.com>
2022-10-18 11:59:35 +0800

    Add files via upload

--- week-5/data.sql
+++ week-5/data.sql
@@ -42,6 +42,35 @@ LOCK TABLES `member` WRITE;
 INSERT INTO `member` VALUES (1,'test2','test','test',0,'2022-10-17 15:45:50'),(2,'Nick','Nickaccount','nick123',0,'2022-10-17 16:07:55'),(3,'angel','angelaccount','angel1115',0,'2022-10-17 16:08:36'),(4,'randy','randyaccount','randy1030',0,'2022-10-17 16:10:14'),(5,'Logan','loganaccount','logan1217',0,'2022-10-17 16:51:06');
 /*!40000 ALTER TABLE `member` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `message`
+--
+
+DROP TABLE IF EXISTS `message`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `message` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `member_id` bigint NOT NULL,
+  `content` varchar(255) NOT NULL,
+  `like_count` int unsigned NOT NULL DEFAULT '0',
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `member_id` (`member_id`),
+  CONSTRAINT `message_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `message`
+--
+
+LOCK TABLES `message` WRITE;
+/*!40000 ALTER TABLE `message` DISABLE KEYS */;
+INSERT INTO `message` VALUES (1,1,'一直下雨好煩呀',7,'2022-10-18 10:45:50'),(2,1,'希望趕快放晴',11,'2022-10-18 10:46:30'),(4,1,'99好像去散步',4,'2022-10-18 11:06:45'),(5,4,'檸檬塔好吃',12,'2022-10-18 11:06:45'),(6,2,'我是rick',1,'2022-10-18 11:06:45'),(7,3,'你好嗎',77,'2022-10-18 11:06:45'),(8,5,'好想騎車喔',45,'2022-10-18 11:06:45');
+/*!40000 ALTER TABLE `message` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -52,4 +81,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-10-17 21:31:33
+-- Dump completed on 2022-10-18 11:58:23
